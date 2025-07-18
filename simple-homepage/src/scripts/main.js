635f998d8da5ba84d4ee46866f732fe0c69491c7
ContagiousMoon1155 <contagiousmoon@gmail.com>
2025-07-18 05:34:36 +0000

    c

--- simple-homepage/src/scripts/main.js
+++ simple-homepage/src/scripts/main.js
@@ -1,1 +1,17 @@
-// This file is intentionally left blank.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Simple Homepage</title>
+  <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+  <header>
+    <h1>Hi!</h1>
+  </header>
+  <main>
+    <p>Welcome to my simple homepage. This is some placeholder text for the body. Feel free to customize it!</p>
+  </main>
+  <script src="scripts/main.js"></script>
+</body>
+</html>
